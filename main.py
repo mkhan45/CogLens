ad906c4e20c1ac1175005e794318b34e6ca1b493
Mikail <mikail.khan45@gmail.com>
2019-07-25 15:34:42 +0000

    fixed gitignore stuff and updated papths

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import embed_text as et
 import json
 
 #Load files
-with open("/Users/crystal/repositories/Student_Week3/bag_of_words/dat/stopwords.txt", 'r') as r: #stop words
+with open("stopwords.txt", 'r') as r: #stop words
     stops = []
     for line in r:
         stops += [i.strip() for i in line.split('\t')]
 
-glove50 = KV.load_word2vec_format('/Users/crystal/Desktop/python-workspace/CogWorks2019/glove.6B.50d.txt.w2v', binary=False) #glove embeddings
+glove50 = KV.load_word2vec_format('path/to/glove.w2v', binary=False) #glove embeddings
 
-coco_data = json.load('/Users/crystal/Desktop/python-workspace/CogWorks2019/captions_train2014.json') #Dictionary
+coco_data = json.load('path/to/coco_data.json') #Dictionary
 
 print("Files loaded.")
 
@@ -19,5 +19,5 @@ all_captions = list(coco_data['annotations'][i]['caption'] for i in coco_data['a
 counters = et.to_counters(all_captions)
 vocab = et.to_vocab(counters, stop_words=stops)
 idf = et.to_idf(counters, vocab)
-embedding = et.se_text("mouse flying", glove50, idf, vocab) #change "mouse flying" to whatever caption
 
+#embedding = et.se_text("mouse flying", glove50, idf, vocab) #change "mouse flying" to whatever caption

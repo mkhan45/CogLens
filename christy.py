5c94eac885b7618df1b4e838ecbccf21c605255b
Christy <cjj9000@gmail.com>
2019-07-25 15:27:10 -0400

    added triple, loading, and ui functions

---
+++ christy.py
@@ -0,0 +1,75 @@
+%matplotlib notebook
+from collections import defaultdict
+import matplotlib.pyplot as plt
+from pathlib import Path
+from urllib.request import urlopen
+import json
+import pickle
+import numpy as np
+
+with open("captions_train2014.json", "r") as read_file:
+    captions_train = json.load(read_file)
+    
+with open("resnet18_features.pkl", "rb") as read_file:
+    images_dict = pickle.load(read_file)
+
+keys_array = np.array(images_dict.keys())
+url_dict = dict()
+for image_set in captions_train['images']:
+    if image_set['id'] in keys_array:
+        url_dict[image_set['id']] = image_set['coco_url']
+        
+captions_dict = defaultdict(list)
+captions_list = list()
+for annotation in captions_train['annotations']:
+    if annotation['image_id'] in keys_array:
+        captions_dict[annotation['image_id']].append(annotation)
+        captions_list.append(annotation['caption'])
+        
+def find_matches(query):
+    '''
+    Parameters:
+    --------------------------------
+    query: string
+    
+    Returns
+    --------------------------------
+    id_list: List[int]
+    List of IDs for Images that match the given query
+    '''
+    
+    
+    
+def display_image(ids):
+    '''
+    Parameters:
+    --------------------------------
+    
+    
+    Returns
+    --------------------------------
+    Matploblib of Matching Image Results
+    '''
+    n = np.ceil(np.sqrt(len(ids)))
+    fig, axes = plt.subplots(nrows = n, ncols = n)
+    for i in range(n):
+        for j in range(n):
+            id = ids[i*n+j]
+            data = urlopen(url_dict[id])
+            # converting the downloaded bytes into a numpy-array
+            img = plt.imread(data, format='jpg')
+            axes[i,j].imshow(img)
+            
+def create_triples(ids):
+    triples_list = list()
+    for id in ids:
+        good_img = images_dict[id]
+        n = np.random.choice(keys_array)
+        while n==id:
+            n = np.random.randint()
+        good_img = images_dict[id]
+        bad_img = images_dict[n]
+        #replace embed_dict with what dictionary contains embeddings for captions
+        caption = embed_dict[id][int(np.random.randint(5,10))]
+        triples_list.append(caption, good_img, bad_img)
+        return triples_list
